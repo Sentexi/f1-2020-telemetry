99b21fcb9d896aafaf3ff585cf9bd95d02989977
Andrey Belkin <communicationbel@gmail.com>
2021-03-30 21:14:46 +0200

    added package description

--- main.py
+++ main.py
@@ -68,5 +68,6 @@ while True:
     '''
 
     if pkg_header[4] == 0 or  pkg_header[4] == 6 or pkg_header[4] == 7:
+        print("packageID = {}".format(pkg_header[4]))
         ar = np.array(unpack(H.packages[pkg_header[4]],data)[10:]) #remove first 10 entries, aka the header
         print(ar)
