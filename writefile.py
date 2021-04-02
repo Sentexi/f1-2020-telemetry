fb13445220f282188aa4327081f4d47d6ae1d0c6
Andrey Belkin <communicationbel@gmail.com>
2021-04-02 20:16:08 +0200

    added utf-8 encoding to write file

--- writefile.py
+++ writefile.py
@@ -4,7 +4,7 @@ import os
 def write_csv(package_ID,data,sessionname):
     Filename = os.path.join(sessionname,str(package_ID),"data")
 
-    with open('{}.csv'.format(Filename),'a',newline='\n') as file:
+    with open('{}.csv'.format(Filename),'a',newline='\n',encoding='utf-8') as file:
         writer = csv.writer(file, delimiter=",",quoting=csv.QUOTE_MINIMAL)
         writer.writerow(data)
     pass
